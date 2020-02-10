43c40ef80be6f8fca64e254eef76301d0b55f0dd
Scott Bennett <stt.btt@gmail.com>
2020-02-10 18:46:07 -0500

    Complete Child View Controller - UserInfoHeaderVC.

---
+++ GHFollowers/Custom Views/ViewControllers/GFUserInfoHeaderVC.swift
@@ -0,0 +1,102 @@
+//
+//  GFUserInfoHeaderVC.swift
+//  GHFollowers
+//
+//  Created by Scott Bennett on 2/10/20.
+//  Copyright © 2020 Scott Bennett. All rights reserved.
+//
+
+import UIKit
+
+class GFUserInfoHeaderVC: UIViewController {
+    
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .left, fontSize: 34)
+    let nameLabel = GFSecondaryTitleLabel(fontSize: 18)
+    let loacationImageView = UIImageView()
+    let locationLabel = GFSecondaryTitleLabel(fontSize: 18)
+    let bioLabel = GFBodyLabel(textAlignment: .left)
+    
+    var user: User!
+    
+    
+    init(user: User) {
+        super.init(nibName: nil, bundle: nil)
+        self.user = user
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSubviews()
+        layoutUI()
+        configureUIElements()
+    }
+    
+    
+    func configureUIElements() {
+        avatarImageView.downloadImage(from: user.avatarUrl)
+        usernameLabel.text = user.login
+        nameLabel.text = user.name ?? ""
+        locationLabel.text = user.location ?? "No Location"
+        bioLabel.text = user.bio ?? "No bio available"
+        bioLabel.numberOfLines = 3
+        
+        loacationImageView.image = UIImage(systemName: SFSymbols.location)
+        loacationImageView.tintColor = .secondaryLabel
+    }
+    
+    
+    func addSubviews() {
+        view.addSubview(avatarImageView)
+        view.addSubview(usernameLabel)
+        view.addSubview(nameLabel)
+        view.addSubview(loacationImageView)
+        view.addSubview(locationLabel)
+        view.addSubview(bioLabel)
+    }
+    
+    
+    func layoutUI() {
+        let padding: CGFloat = 20
+        let textImagePadding: CGFloat = 12
+        loacationImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 90),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 90),
+            
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.topAnchor),
+            usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: textImagePadding),
+            usernameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 38),
+            
+            nameLabel.centerYAnchor.constraint(equalTo: avatarImageView.centerYAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: textImagePadding),
+            nameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            nameLabel.heightAnchor.constraint(equalToConstant: 20),
+            
+            loacationImageView.bottomAnchor.constraint(equalTo: avatarImageView.bottomAnchor),
+            loacationImageView.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: textImagePadding),
+            loacationImageView.widthAnchor.constraint(equalToConstant: 20),
+            loacationImageView.heightAnchor.constraint(equalToConstant: 20),
+            
+            locationLabel.centerYAnchor.constraint(equalTo: loacationImageView.centerYAnchor),
+            locationLabel.leadingAnchor.constraint(equalTo: loacationImageView.trailingAnchor, constant: 5),
+            locationLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            locationLabel.heightAnchor.constraint(equalToConstant: 20),
+            
+            bioLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: textImagePadding),
+            bioLabel.leadingAnchor.constraint(equalTo: avatarImageView.leadingAnchor),
+            bioLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            bioLabel.heightAnchor.constraint(equalToConstant: 60)
+        ])
+    }
+}
---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Scott Bennett on 2/10/20.
+//  Copyright © 2020 Scott Bennett. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
