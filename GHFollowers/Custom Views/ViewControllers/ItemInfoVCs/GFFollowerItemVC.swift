adb8facd14e049cc40477f8481cdeb7bd8da9994
Scott Bennett <stt.btt@gmail.com>
2020-02-11 10:45:25 -0500

    Complete ItemInfoVC - Subclasses.

---
+++ GHFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
@@ -0,0 +1,24 @@
+//
+//  GFFollowerItemVC.swift
+//  GHFollowers
+//
+//  Created by Scott Bennett on 2/11/20.
+//  Copyright © 2020 Scott Bennett. All rights reserved.
+//
+
+import UIKit
+
+class GFFollowerItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .followers, with: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, with: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+}
---
+++ GHFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFRepoItemVC.swift
@@ -0,0 +1,24 @@
+//
+//  GFRepoItemVC.swift
+//  GHFollowers
+//
+//  Created by Scott Bennett on 2/11/20.
+//  Copyright © 2020 Scott Bennett. All rights reserved.
+//
+
+import UIKit
+
+class GFRepoItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, with: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, with: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "GitHub Profile")
+    }
+}
